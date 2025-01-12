ff899a864180a2f04f7f13971eb41457567c8d9f
gl-aarav <gl.aarav@gmail.com>
2025-01-12 12:45:42 -0800

    Updates

---
+++ src/PhoneBook.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class PhoneBook
+{
+    private String name, city, phone;
+
+    public PhoneBook()
+    {
+        // Default values
+        name = "Thomas Jefferson";
+        city = "New York";
+        phone = "123-4567";
+    }
+
+    public static void main(String[] args)
+    {
+        PhoneBook book = new PhoneBook();
+        book.getInput();
+        book.processInputAndPrint();
+    }
+
+    public void getInput()
+    {
+        Scanner in = new Scanner(System.in);
+
+        // Get user input for name, city, and phone number
+        System.out.print("Enter name as first last: ");
+        name = in.nextLine();
+
+        System.out.print("Enter city of residence: ");
+        city = in.nextLine();
+
+        System.out.print("Enter phone number (without area code): ");
+        phone = in.nextLine();
+    }
+
+    public void processInputAndPrint()
+    {
+        // Extract first and last name
+        String[] nameParts = name.split(" ");
+        String firstName = nameParts[0];
+        String lastName = nameParts[1];
+
+        // Determine the area code based on the city
+        String areaCode = "";
+
+        if (city.equalsIgnoreCase("Mountain View") || city.equalsIgnoreCase("Palo Alto"))
+        {
+            areaCode = "(650)";
+        }
+        else if (city.equalsIgnoreCase("Cupertino") || city.equalsIgnoreCase("San Jose"))
+        {
+            areaCode = "(408)";
+        }
+
+        // Format phone number with area code
+        String formattedPhone = areaCode + " " + phone;
+
+        // Alphabetize by last name initial
+        char lastInitial = lastName.charAt(0);
+
+        // Print the formatted information
+        System.out.println("The phone number will be alphabetized under " + lastInitial +
+                           ", under " + lastName + ", " + firstName + " at " + formattedPhone + ".");
+    }
+}
