07d4e0841d8ccab0811ed92a5b859277cd973b31
gl-aarav <gl.aarav@gmail.com>
2025-01-25 10:57:11 -0800

    Update PhoneBook.java

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,26 +1,22 @@
 import java.util.Scanner;
 
-public class PhoneBook
-{
+public class PhoneBook {
     private String name, city, phone;
 
-    public PhoneBook()
-    {
+    public PhoneBook() {
         // Default values
         name = "Thomas Jefferson";
         city = "New York";
         phone = "123-4567";
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         PhoneBook book = new PhoneBook();
         book.getInput();
         book.processInputAndPrint();
     }
 
-    public void getInput()
-    {
+    public void getInput() {
         Scanner in = new Scanner(System.in);
 
         // Get user input for name, city, and phone number
@@ -34,22 +30,20 @@ public class PhoneBook
         phone = in.nextLine();
     }
 
-    public void processInputAndPrint()
-    {
-        // Extract first and last name
-        String[] nameParts = name.split(" ");
-        String firstName = nameParts[0];
-        String lastName = nameParts[1];
+    public void processInputAndPrint() {
+        // Extract first and last name using indexOf and substring methods
+        int spaceIndex = name.indexOf(" "); // Find where the first and last name are separated
+
+        String firstName = name.substring(0, spaceIndex); // First name is before the space
+        String lastName = name.substring(spaceIndex + 1); // Last name is after the space
 
         // Determine the area code based on the city
         String areaCode = "";
 
-        if (city.equalsIgnoreCase("Mountain View") || city.equalsIgnoreCase("Palo Alto"))
-        {
+        if (city.equalsIgnoreCase("Mountain View") || city.equalsIgnoreCase("Palo Alto")) {
             areaCode = "(650)";
-        }
-        else if (city.equalsIgnoreCase("Cupertino") || city.equalsIgnoreCase("San Jose"))
-        {
+        } 
+        else if (city.equalsIgnoreCase("Cupertino") || city.equalsIgnoreCase("San Jose")) {
             areaCode = "(408)";
         }
 
